aac07741c25663c72c090042b1c3c13ef908211d
Andreas G <13819164+agaertner@users.noreply.github.com>
2023-04-10 00:57:39 +0200

    chore: add KpWebApiService

---
+++ src/Core/Services/KpWebApiService.cs
@@ -0,0 +1,15 @@
+﻿using Nekres.ProofLogix.Core.Services.KpWebApi.V1.Models;
+using Nekres.ProofLogix.Core.Utils;
+using System.Threading.Tasks;
+
+namespace Nekres.ProofLogix.Core.Services {
+    internal class KpWebApiService {
+
+        private string _uri = "https://killproof.me/api/";
+
+        public async Task<Account> GetAccount(string id) {
+            var account = await TaskUtil.RetryAsync<Account>($"{_uri}kp/{id}");
+            return account;
+        }
+    }
+}
