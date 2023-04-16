157b4d5dde7d3586c9e70c8e0ff5e498d34b34af
Andreas G <13819164+agaertner@users.noreply.github.com>
2023-04-16 04:26:44 +0200

    fix: obsolete converter reference

--- src/Core/Services/KpWebApi/V1/Models/Clear.cs
+++ src/Core/Services/KpWebApi/V1/Models/Clear.cs
@@ -7,7 +7,7 @@ namespace Nekres.ProofLogix.Core.Services.KpWebApi.V1.Models {
     // Paths: /api/clear/{account_name_OR_kp_id}
     //        /api/character/{character_name}/clear
 
-    [JsonConverter(typeof(RaidConverter))]
+    [JsonConverter(typeof(ClearConverter))]
     public sealed class Clear : BaseResponse {
         public string Name { get; set; }
 
