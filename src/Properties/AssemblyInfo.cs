1c5409c759e2ada7f70c4f2037c6fcd4bc1c9099
GitHub Action <action@github.com>
2023-08-12 19:45:19 +0000

    (skip): 🔖 bump version to 2.18.7-dev.158

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -18,6 +18,6 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.18.5.154")]
-[assembly: AssemblyFileVersion("2.18.5.154")]
-[assembly: AssemblyInformationalVersion("2.18.5-dev.154+Branch.dev.Sha.a20df3f0bdf4a2bf8e89ba4a0ab45724923a3d1e")]
+[assembly: AssemblyVersion("2.18.7.158")]
+[assembly: AssemblyFileVersion("2.18.7.158")]
+[assembly: AssemblyInformationalVersion("2.18.7-dev.158+Branch.dev.Sha.0038e4c83812ac67a01d7467aa4b282c31e37f75")]
